95153f2616023c7c785398339c0a5cef56671852
Malek Bakeer - Shusmo <malek@shusmo.io>
2025-08-09 13:24:09 +0300

    Admin

---
+++ admin/firebase-config.js
@@ -0,0 +1,11 @@
+// /admin/firebase-config.js
+// Fill these from your Firebase Console -> Project Settings -> Your apps (Web app)
+export const firebaseConfig = {
+  apiKey: "AIzaSyBG9iWsbLr_7uQiLFv3lLo8gxRo7QRRfFc",
+  authDomain: "shusmowebsite.firebaseapp.com",
+  projectId: "shusmowebsite",
+  storageBucket: "shusmowebsite.firebasestorage.app",
+  messagingSenderId: "746430553004",
+  appId: "1:746430553004:web:5c1aea7551da0a7e683809",
+  measurementId: "G-02CENV1W5J"
+};
