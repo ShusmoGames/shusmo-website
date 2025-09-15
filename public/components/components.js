25a869dc54f9aca380d0c6f42a4eecf409465595
Danaalsawalha <67854925+Danaalsawalha@users.noreply.github.com>
2025-09-15 18:55:44 +0300

    UI

--- public/components/components.js
+++ public/components/components.js
@@ -89,3 +89,19 @@ document.addEventListener('DOMContentLoaded', function() {
         loadComponent('#footer-placeholder', 'components/footer.html');
     }
 });
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  };
